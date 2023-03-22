0ca31ea86de249f1cb5a4cf5d16fb52924210c6c
PICQ Estelle <estelle.picq@altran.com>
2023-03-22 18:31:17 +0100

    chore: remove comments

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -22,15 +22,7 @@ export class Tab1Page {
       allowEditing: true,
       resultType: CameraResultType.Uri
     });
-  
-    // image.webPath will contain a path that can be set as an image src.
-    // You can access the original file using image.path, which can be
-    // passed to the Filesystem API to read the raw data of the image,
-    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
     var imageUrl = image.webPath || '';
-    
-  
-    // Can be set to the src of an image now
     this.imageUrl = this.sanitizer.bypassSecurityTrustUrl(imageUrl);
   }
 
